6c15b42b755fc5f5b004e948612ecc8f35a014ff
Kyle Mosier <mosierkyle@gmail.com>
2023-11-29 11:56:19 -0800

    fix logout bug and edit logout btn css

--- public/scripts/dashboardScript.js
+++ public/scripts/dashboardScript.js
@@ -1,13 +1,9 @@
 const logoutForm = document.querySelector('#logout-form');
 const logoutDiv = document.querySelector('#logout-div');
 
-const submitLogoutForm = () => {
-  logoutForm.submit();
-};
-
 const addEvents = () => {
   logoutDiv.addEventListener('click', () => {
-    submitLogoutForm();
+    logoutForm.submit();
   });
 };
 
