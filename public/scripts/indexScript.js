e56dd5f0e02c9d9f40db2cd89e35007bfcf6e246
Kyle Mosier <mosierkyle@gmail.com>
2023-11-30 11:13:13 -0800

    fix login button in hamburger menu

--- public/scripts/indexScript.js
+++ public/scripts/indexScript.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('#mobile');
 const linkList = document.querySelector('#links-list');
 const x = document.querySelector('#exit');
 const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+const login = document.querySelector('#login');
 
 const events = () => {
   for (let i = 0; i < navLinks.length; i++) {
@@ -17,6 +18,9 @@ const events = () => {
 
   hamburger.addEventListener('click', () => {
     linkList.style.top = '0';
+    // login.style.backgroundColor = 'black';
+    // login.style.padding = '0.5rem 0';
+    // login.style.listStyle = '0.5rem 0';
   });
 
   x.addEventListener('click', () => {
