212c4dad5f7c4166b94e5925a3c086d17293823d
Kyle Mosier <mosierkyle@gmail.com>
2023-12-08 17:33:58 -0800

    Troubleshoot

--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -306,8 +306,7 @@ const generateWorkoutPDF = async (req, res) => {
     // Generate PDF using Puppeteer
     const browser = await puppeteer.launch({
       headless: 'new',
-      executablePath:
-        '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome',
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/',
     });
     const page = await browser.newPage();
     await page.setContent(htmlTemplateFitness, {
@@ -349,8 +348,7 @@ const generateDietPDF = async (req, res) => {
     // Generate PDF using Puppeteer
     const browser = await puppeteer.launch({
       headless: 'new',
-      executablePath:
-        '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome',
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/',
     });
     const page = await browser.newPage();
     await page.setContent(htmlTemplateDiet, { waitUntil: 'domcontentloaded' });
@@ -386,8 +384,7 @@ const generateNutritionPDF = async (req, res) => {
     // Generate PDF using Puppeteer
     const browser = await puppeteer.launch({
       headless: 'new',
-      executablePath:
-        '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome',
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/',
     });
     const page = await browser.newPage();
     await page.setContent(htmlTemplateNutrition, {
